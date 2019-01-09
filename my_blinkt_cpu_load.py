822ce590cd65f20f6476963246efa140d5bbe11c
meise <sebastianmeisner@gmx.de>
2019-01-09 23:01:01 +0100

    - added functions to convert 0 to 1.0 to grenn to red color scale

--- my_blinkt_cpu_load.py
+++ my_blinkt_cpu_load.py
@@ -2,6 +2,27 @@
 
 import time
 from sys import exit
+import math
+
+def floatRgb(mag, cmin, cmax):
+    """ Return a tuple of floats between 0 and 1 for R, G, and B. """
+    # Normalize to 0-1
+    try: x = float(mag-cmin)/(cmax-cmin)
+    except ZeroDivisionError: x = 0.5 # cmax == cmin
+    blue  = min((max((4*(0.75-x), 0.)), 1.))
+    red   = min((max((4*(x-0.25), 0.)), 1.))
+    green = min((max((4*math.fabs(x-0.5)-1., 0.)), 1.))
+    return red, green, blue
+
+
+def rgb(mag, cmin, cmax):
+    """ Return a tuple of integers, as used in AWT/Java plots. """
+    red, green, blue = floatRgb(mag, cmin, cmax)
+    return int(red*255), int(green*255), int(blue*255)
+
+def strRgb(mag, cmin, cmax):
+    """ Return a hex string, as used in Tk plots. """
+    return "#%02x%02x%02x" % rgb(mag, cmin, cmax)
 
 try:
     import psutil
